67c04bd824964bc2d9bf251a2d1162fd28e9c18b
Oleg Pervushin <alec.ext@gmail.com>
2021-01-16 22:01:38 +0400

    enabled uuid extensions

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,10 @@ module TemplateRailsApiApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.active_record.primary_key = :uuid
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+      g.orm :active_record, foreign_key_type: :uuid
+    end
   end
 end
